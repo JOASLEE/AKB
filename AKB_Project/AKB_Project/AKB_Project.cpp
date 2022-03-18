82064f5f2fbefd7c6ef3126a379fe91d36423492
KOJIEUN <0930ko@gmail.com>
2022-03-18 13:21:20 +0900

    Add project kje

--- AKB_Project/AKB_Project/AKB_Project.cpp
+++ AKB_Project/AKB_Project/AKB_Project.cpp
@@ -8,6 +8,13 @@ int main()
     std::cout << "Hello World!\n";
 }
 
+
+int main()
+{
+    std::cout << "1111Hello World!\n";
+}
+
+
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
 // 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
